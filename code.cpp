eb45106dcf8674da64e0ef1c301303033db512a5
Rasel477867 <113754632+Rasel477867@users.noreply.github.com>
2023-08-19 09:35:33 +0600

    A. Replacing Elements-codeforces-solution
    https://codeforces.com/contest/1473/problem/A

--- code.cpp
+++ code.cpp
@@ -12,37 +12,37 @@ const ll mod=1e9+7;
 using namespace std;
 vector<ll>v;
  string sub;
-bool cheak(ll n,string s,string s1)
-{
-    set<string>s2;
-
-    ll i;
-    if(s.size()%n!=0 || s.size()<n)
-        return false;
-    for(i=0; i<s.size()-n+1; i=i+n)
-    {
-        sub=s.substr(i,n);
-
-       if(s2.size()>1)
-          return false;
-        s2.insert(sub);
-
-    }
-    if(s1.size()%n!=0 || s1.size()<n)
-        return false;
-    for(i=0; i<s1.size()-n+1; i+=n)
-    {
-        sub=s1.substr(i,n);
-
-        if(s2.size()>1)
-            return false;
-        s2.insert(sub);
-    }
-    if(s2.size()==1)
-        return true;
-    else
-        return false;
-}
+//bool cheak(ll n,string s,string s1)
+//{
+//    set<string>s2;
+//
+//    ll i;
+//    if(s.size()%n!=0 || s.size()<n)
+//        return false;
+//    for(i=0; i<s.size()-n+1; i=i+n)
+//    {
+//        sub=s.substr(i,n);
+//
+//       if(s2.size()>1)
+//          return false;
+//        s2.insert(sub);
+//
+//    }
+//    if(s1.size()%n!=0 || s1.size()<n)
+//        return false;
+//    for(i=0; i<s1.size()-n+1; i+=n)
+//    {
+//        sub=s1.substr(i,n);
+//
+//        if(s2.size()>1)
+//            return false;
+//        s2.insert(sub);
+//    }
+//    if(s2.size()==1)
+//        return true;
+//    else
+//        return false;
+//}
 //ll ex(ll a,ll b,ll mod)
 //{
 //    if(b==0)
@@ -70,36 +70,31 @@ int main()
     cin.tie(0);
     cout.tie(0);
     ll j,i,t;
-    ll n,a,b,p,y;
-    string s,s1;
+     ll n,a,b,y,k;
     cin>>t;
     while(t--)
     {
-        cin>>s>>s1;
+        cin>>n>>k;
+        v.clear();
         y=0;
-        a=s.size();
-        b=s1.size();
-        n=min(a,b);
         for(i=0; i<n; i++)
         {
-            if(cheak(i+1,s,s1))
-            {
-                p=i+1;
+            cin>>a;
+            if(a>k)
                 y=1;
-                break;
-            }
+            v.pb(a);
         }
+        sort(v.begin(),v.end());
         if(y==0)
-            cout<<-1<<endl;
+            cout<<"YES"<<endl;
         else
-            {
-              a=gcd(s.size(),s1.size());
-              b=(s.size()*s1.size())/a;
-              n=sub.size();
-              for(i=1; i<=b;i+=n)
-                cout<<sub;
-              cout<<endl;
-            }
+        {
+            a=v[0]+v[1];
+            if(a<=k)
+                cout<<"YES"<<endl;
+            else
+                cout<<"NO"<<endl;
+        }
     }
     return 0;
 }
