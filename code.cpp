0274717b53c26407597985bb4f4af47868f63fb6
Rasel477867 <113754632+Rasel477867@users.noreply.github.com>
2023-08-19 09:16:33 +0600

    B. String LCM-codeforces-solution
    https://codeforces.com/problemset/problem/1473/B

--- code.cpp
+++ code.cpp
@@ -10,8 +10,39 @@
 #define pf push_front
 const ll mod=1e9+7;
 using namespace std;
+vector<ll>v;
+ string sub;
+bool cheak(ll n,string s,string s1)
+{
+    set<string>s2;
 
+    ll i;
+    if(s.size()%n!=0 || s.size()<n)
+        return false;
+    for(i=0; i<s.size()-n+1; i=i+n)
+    {
+        sub=s.substr(i,n);
 
+       if(s2.size()>1)
+          return false;
+        s2.insert(sub);
+
+    }
+    if(s1.size()%n!=0 || s1.size()<n)
+        return false;
+    for(i=0; i<s1.size()-n+1; i+=n)
+    {
+        sub=s1.substr(i,n);
+
+        if(s2.size()>1)
+            return false;
+        s2.insert(sub);
+    }
+    if(s2.size()==1)
+        return true;
+    else
+        return false;
+}
 //ll ex(ll a,ll b,ll mod)
 //{
 //    if(b==0)
@@ -21,17 +52,17 @@ using namespace std;
 //    else
 //        return (a*ex((a*a)%mod,(b-1)/2,mod))%mod;
 //}
-//long long int gcd(long long int a,long long int b)
-//{
-//    long long int c;
-//    while(a%b!=0)
-//    {
-//        c=b;
-//        b=a%b;
-//        a=c;
-//    }
-//    return b;
-//}
+long long int gcd(long long int a,long long int b)
+{
+    long long int c;
+    while(a%b!=0)
+    {
+        c=b;
+        b=a%b;
+        a=c;
+    }
+    return b;
+}
 
 int main()
 {
@@ -39,31 +70,36 @@ int main()
     cin.tie(0);
     cout.tie(0);
     ll j,i,t;
-    ll n,a,b,num,y,m;
+    ll n,a,b,p,y;
+    string s,s1;
     cin>>t;
     while(t--)
     {
-        cin>>n>>m;
-        num=n+m;
-        a=n-m;
-        if(a!=1)
-            cout<<"NO"<<endl;
-        else{
+        cin>>s>>s1;
         y=0;
-        for(i=2; i*i<=num; i++)
+        a=s.size();
+        b=s1.size();
+        n=min(a,b);
+        for(i=0; i<n; i++)
         {
-            if(num%i==0)
+            if(cheak(i+1,s,s1))
             {
+                p=i+1;
                 y=1;
                 break;
             }
         }
         if(y==0)
-            cout<<"YES"<<endl;
+            cout<<-1<<endl;
         else
-            cout<<"NO"<<endl;
-        }
+            {
+              a=gcd(s.size(),s1.size());
+              b=(s.size()*s1.size())/a;
+              n=sub.size();
+              for(i=1; i<=b;i+=n)
+                cout<<sub;
+              cout<<endl;
+            }
     }
-
     return 0;
 }
