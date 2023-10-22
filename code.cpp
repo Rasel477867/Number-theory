138c37be01e7b866b2e1e1f892f37bc9e6cc7239
Rasel477867 <113754632+Rasel477867@users.noreply.github.com>
2023-10-22 10:27:21 +0600

    Proper Nutrition-codeforces-problem
    https://codeforces.com/problemset/problem/898/B

--- code.cpp
+++ code.cpp
@@ -1,100 +1,52 @@
-
 #include <ext/pb_ds/assoc_container.hpp> // Common file
 #include <ext/pb_ds/tree_policy.hpp>
 #include <functional> // for less
 #include <iostream>
 #include<bits/stdc++.h>
 #define endl '\n'
-#define ll long long int
+#define ll  long long int
 #define pb push_back
-#define pf push_front
-const ll mod=1e9+7;
-using namespace std;
-vector<ll>v;
- string sub;
-//bool cheak(ll n,string s,string s1)
-//{
-//    set<string>s2;
-//
-//    ll i;
-//    if(s.size()%n!=0 || s.size()<n)
-//        return false;
-//    for(i=0; i<s.size()-n+1; i=i+n)
-//    {
-//        sub=s.substr(i,n);
-//
-//       if(s2.size()>1)
-//          return false;
-//        s2.insert(sub);
-//
-//    }
-//    if(s1.size()%n!=0 || s1.size()<n)
-//        return false;
-//    for(i=0; i<s1.size()-n+1; i+=n)
-//    {
-//        sub=s1.substr(i,n);
-//
-//        if(s2.size()>1)
-//            return false;
-//        s2.insert(sub);
-//    }
-//    if(s2.size()==1)
-//        return true;
-//    else
-//        return false;
-//}
-//ll ex(ll a,ll b,ll mod)
-//{
-//    if(b==0)
-//        return 1;
-//    else if(b%2==0)
-//        return (ex((a*a)%mod,b/2,mod));
-//    else
-//        return (a*ex((a*a)%mod,(b-1)/2,mod))%mod;
-//}
-long long int gcd(long long int a,long long int b)
-{
-    long long int c;
-    while(a%b!=0)
-    {
-        c=b;
-        b=a%b;
-        a=c;
-    }
-    return b;
-}
 
+
+using namespace __gnu_pbds;
+using namespace std;
+deque<ll>v;
+set<ll>s;
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    ll j,i,t;
-     ll n,a,b,y,k;
-    cin>>t;
-    while(t--)
+    ll i,j,t;
+    ll n,a,ans,y,ans1,b,num;
+    cin>>n>>a>>b;
+    y=1;
+    for(i=0; ; i++)
     {
-        cin>>n>>k;
-        v.clear();
-        y=0;
-        for(i=0; i<n; i++)
+        num=n-b*i;
+        if(num<0)
         {
-            cin>>a;
-            if(a>k)
-                y=1;
-            v.pb(a);
+            y=0;
+            break;
         }
-        sort(v.begin(),v.end());
-        if(y==0)
-            cout<<"YES"<<endl;
-        else
+        if(num%a==0)
         {
-            a=v[0]+v[1];
-            if(a<=k)
-                cout<<"YES"<<endl;
-            else
-                cout<<"NO"<<endl;
+            ans=num/a;
+            ans1=i;
+            break;
         }
     }
+    if(y==0)
+        cout<<"NO"<<endl;
+    else
+    {
+        cout<<"YES"<<endl;
+        cout<<ans<<" "<<ans1<<endl;
+    }
+
+
+
     return 0;
 }
+
+
